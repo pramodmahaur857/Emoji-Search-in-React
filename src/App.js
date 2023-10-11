f6ce2c6c7b28f44f95bc214ee0dbf01c32bdc097
pramodmahaur857 <pramodmahaur857@gmail.com>
2023-10-11 19:31:04 +0530

    Emoji search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { useState } from 'react'
+import './App.css'
+import Emoji from "./Emoji.js"
+
+export default function App() {
+  const [emojidata,setemojidata] = useState(Emoji)
+  
+  const findEmoji = (emojiName) =>{
+    const filteremoji = Emoji.filter((item)=>{
+      if(item.name.toString().toLowerCase().includes(emojiName.toLowerCase())){
+        return (emojiName ==='' || item.name.toString().toLowerCase().includes(emojiName.toLowerCase()))}
+      else{
+        return false
+        }
+    }) 
+    console.log(emojidata)
+    setemojidata(filteremoji)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div className="maincontainer">
+        <div className="hr"></div>
+        <div className="emoji"> 🐱 Emoji search 🐱</div>
+      <div>
+        <input type="text" placeholder="Search.."  onChange={(e)=>findEmoji(e.target.value)}></input>
+      </div>
+      <div>
+        {emojidata && emojidata.map((item)=>{
+          return <div className="mapdiv" key={item.id}>
+              <div>{item.emoji} {item.name}</div>
+          </div>
+        })}
+        {emojidata.length===0 && (<div>Emoji not found</div>)}
+      </div>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+
---
+++ src/Emoji.js
@@ -0,0 +1,21 @@
+const Emoji = [
+    {id:1,emoji:"💯",name:100},
+    {id:2,emoji:"🔢",name:1234},
+    {id:3,emoji:"😃",name:"Griming"},
+    {id:4,emoji:"😬",name:"Gramacing"},
+    {id:5,emoji:"😁",name:"Grin"},
+    {id:6,emoji:"😂",name:"Joy"},
+    {id:7,emoji:"😀",name:"Smicy"},
+    {id:8,emoji:"😁",name:"Smile"},
+    {id:9,emoji:"😄",name:"Sweet smile"},
+    {id:10,emoji:"😅",name:"Laughing"},
+    {id:11,emoji:"😛",name:"Face with tongue"},
+    {id:12,emoji:"😙",name:"Kissing face"},
+    {id:13,emoji:"😢",name:"Crying face"},
+    {id:14,emoji:"😡",name:"Pouting face"},
+    {id:15,emoji:"😘",name:"Face blowign kis"},
+    {id:16,emoji:"🧡",name:"orange heart"},
+    {id:17,emoji:"💔",name:"Broken heart"},
+    {id:18,emoji:"🤪",name:"Zany face"}
+  ]
+  export default Emoji;
